c17dc10423aa60f868bc0b8555c0cf37419bb887
Leonardo Gago <ljgago@gmail.com>
2020-08-19 19:56:30 -0300

    I add some transforms
    I add Normalizer, StandardScaler and MinMaxScaler pre-processing transforms functions

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +15,54 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+    class Normalize(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, ignore_columns):
+        self.columns = columns
+        self.ignore_columns = ignore_columns
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        ct = ColumnTransformer(
+            [("norml2", Normalizer(norm='l2'), data.drop(self.ignore_columns, axis=1).columns)],
+            remainder='passthrough'
+        )
+        data[data.columns] = ct.fit_transform(X=data)
+        return data
+
+class standardize(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, ignore_columns):
+        self.columns = columns
+        self.ignore_columns = ignore_columns
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        ct = ColumnTransformer(
+            [("std", StandardScaler(), data.drop(self.ignore_columns, axis=1).columns)],
+            remainder='passthrough'
+        )
+        data[data.columns] = ct.fit_transform(X=data)
+        return data
+
+class MinMax(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, ignore_columns):
+        self.columns = columns
+        self.ignore_columns = ignore_columns
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+        ct = ColumnTransformer(
+            [("minmax", MinMaxScaler(), data.drop(self.ignore_columns, axis=1).columns)],
+            remainder='passthrough'
+        )
+        data[data.columns] = ct.fit_transform(X=data)
+        return data
